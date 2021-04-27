f41b2afeac2af380aac64aad8d3bf8847f33935b
Alex_Fajar_Tri_Utomo <alex.fajar10@gmail.com>
2021-04-27 20:59:38 +0700

    Mengambil data dari database

--- app/src/main/java/org/d3if4007/hitungbmi/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if4007/hitungbmi/ui/histori/HistoriFragment.kt
@@ -1,13 +1,24 @@
 package org.d3if4007.hitungbmi.ui.histori
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import androidx.lifecycle.observe
 import org.d3if4007.hitungbmi.databinding.FragmentHistoriBinding
+import org.d3if4007.hitungbmi.db.BmiDb
 
 class HistoriFragment: Fragment() {
+
+    private val viewModel: HistoriViewModel by lazy {
+        val db = BmiDb.getInstance(requireContext())
+        val factory = HistoriViewModelFactory(db.dao)
+        ViewModelProvider(this, factory).get(HistoriViewModel::class.java)
+    }
 
     private lateinit var binding: FragmentHistoriBinding
 
@@ -18,5 +29,13 @@ class HistoriFragment: Fragment() {
     ): View? {
         binding = FragmentHistoriBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.data.observe(viewLifecycleOwner, {
+            Log.d("HistoriFragment", "Jumlah data: ${it.size}")
+        })
     }
 }
