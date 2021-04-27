8091ded4061c917093ea0397de5420d9a6eab873
Alex_Fajar_Tri_Utomo <alex.fajar10@gmail.com>
2021-04-27 19:35:59 +0700

    Memperbaiki navigasi dengan view model

--- app/src/main/java/org/d3if4007/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if4007/hitungbmi/ui/hitung/HitungFragment.kt
@@ -1,4 +1,4 @@
-package org.d3if4007.hitungbmi.ui
+package org.d3if4007.hitungbmi.ui.hitung
 
 import android.content.Intent
 import android.os.Bundle
@@ -13,20 +13,18 @@ import androidx.navigation.fragment.findNavController
 import org.d3if4007.hitungbmi.R
 import org.d3if4007.hitungbmi.data.KategoriBmi
 import org.d3if4007.hitungbmi.databinding.FragmentHitungBinding
+import org.d3if4007.hitungbmi.ui.hitung.HitungFragmentDirections
 
 class HitungFragment : Fragment() {
 
     private val viewModel : HitungViewModel by viewModels()
     private lateinit var binding: FragmentHitungBinding
-    private lateinit var kategoriBmi: KategoriBmi
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
         binding.button.setOnClickListener { hitungBmi() }
         binding.button2.setOnClickListener { reset() }
-        binding.saranButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi))
-        }
+        binding.saranButton.setOnClickListener { viewModel.mulaiNavigasi()}
         binding.shareButton.setOnClickListener { shareData() }
         setHasOptionsMenu(true)
         return binding.root
@@ -34,6 +32,12 @@ class HitungFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getNavigasi().observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(it))
+            viewModel.selesaiNavigasi()
+        })
 
         viewModel.getHasilBmi().observe(viewLifecycleOwner, {
             if (it == null) return@observe
